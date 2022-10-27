f911b5f49b614ef1d34114156c6af3230e5a435b
edermaciel <64818213+edermaciel@users.noreply.github.com>
2022-10-26 21:05:37 -0300

    Carousel working and Acomodações section added

--- src/app/components/restrict/restrict.module.ts
+++ src/app/components/restrict/restrict.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { RestrictComponent } from './restrict.component';
 import { NavbarModule } from '../navbar';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CarouselModule } from '../carousel';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   imports: [
     CommonModule,
     NavbarModule,
+    CarouselModule,
     NgbModule
   ]
 })
