251b14c9ed919c0390b7e65c258774595fe9469e
zedemac <zedemac@ericsson.com>
2023-01-12 13:47:04 -0300

    map working

--- src/app/components/routes/localization/localization.component.ts
+++ src/app/components/routes/localization/localization.component.ts
@@ -1,15 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
+import * as L from 'leaflet';
+import { Observable, Subscriber } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 @Component({
   selector: 'app-localization',
   templateUrl: './localization.component.html',
   styleUrls: ['./localization.component.less']
 })
-export class LocalizationComponent implements OnInit {
+export class LocalizationComponent implements OnInit, AfterViewInit {
+  map: any;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => { this.loadMap(); }, 1000);
+  }
+
+  private getCurrentPosition(): any {
+    return new Observable((observer: Subscriber<any>) => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position: any) => {
+          observer.next({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          });
+          observer.complete();
+        });
+      } else {
+        observer.error();
+      }
+    });
+  }
+
+  private loadMap(): void {
+    this.map = L.map('map').setView([0, 0], 1);
+    L.tileLayer(`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${environment.mapbox.accessToken}`, {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox/streets-v11',
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: environment.mapbox.accessToken,
+    }).addTo(this.map);
+
+    this.getCurrentPosition()
+      .subscribe((position: any) => {
+        this.map.flyTo([position.latitude, position.longitude], 13);
+
+        const icon = L.icon({
+          iconUrl: 'assets/images/marker-icon.png',
+          shadowUrl: 'assets/images/marker-shadow.png',
+          popupAnchor: [13, 0],
+        });
+
+        const marker = L.marker([position.latitude, position.longitude], { icon }).bindPopup('Angular Leaflet');
+        marker.addTo(this.map);
+      });
   }
 
 }
