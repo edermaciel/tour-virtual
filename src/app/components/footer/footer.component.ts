0edae453cfe4485198a6f4d73c1f7ebfbf2eb708
zedemac <zedemac@ericsson.com>
2023-01-13 14:30:25 -0300

    dynamic structure added

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -8,14 +8,32 @@ import { Component, Input, OnInit } from '@angular/core';
 export class FooterComponent implements OnInit {
   @Input('showName') showName: boolean = true;
 
+  linkList: any[] = [
+    {
+      link: 'https://wa.me/5535991304000?text=Olá!%20Gostaria%20de%20fazer%20uma%20reserva%20na%20Pousada%20Doce%20Recanto,%20você%20pode%20me%20ajudar?%20Agradeço desde já!%20:)',
+      image: 'assets/images/whatsapp.png',
+      description: 'Contato do Whatsapp',
+      width: '40',
+      height: '40'
+    },
+    {
+      link: 'https://facebook.com/pousadadocerecanto',
+      image: 'assets/images/facebook.png',
+      description: 'Perfil do Facebook',
+      width: '35',
+      height: '35'
+    },
+    {
+      link: 'https://instagram.com/pousadadocerecanto',
+      image: 'assets/images/instagram.png',
+      description: 'Perfil do Instagram',
+      width: '35',
+      height: '35'
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
   }
-
-  redirectToUrl(event: Event): void {
-    console.log((<HTMLElement>event.target).dataset['value']!);
-    // window.open((<HTMLElement>event.target).dataset['value']!, '_blank')?.focus();
-  }
-
 }
