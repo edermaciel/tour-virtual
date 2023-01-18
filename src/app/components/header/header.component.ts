da861ac6540c59656f2d03ebaa714e12e6ea9777
zedemac <zedemac@ericsson.com>
2023-01-17 21:08:55 -0300

    menu toggle working now

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
+  host: {
+    '(document:click)': 'onClick($event)'
+  },
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.less']
 })
 export class HeaderComponent implements OnInit {
+  @ViewChild('listMenuMobile') listMenuMobile!: ElementRef<HTMLUListElement>;
+  @ViewChild('containerLabel') containerLabel!: ElementRef<HTMLLabelElement>;
+
   listMenu: any[] = [
     { title: 'Contato', link: '/contato' },
     { title: 'Acomodações', link: '/acomodacao' },
@@ -14,9 +20,26 @@ export class HeaderComponent implements OnInit {
     { title: 'Sobre Nós', link: '/sobre-nos' },
   ];
 
-  constructor() { }
+  constructor(private _eref: ElementRef) { }
 
   ngOnInit(): void {
+  }
+
+  onClick(event: any): void {
+    if (!this._eref.nativeElement.contains(event.target)) {
+      this.listMenuMobile!.nativeElement.classList.replace('open-menu', 'close-menu');
+      this.containerLabel!.nativeElement.style.background = '#FFFFFF';
+    }
+  }
+
+  openMenu(): void {
+    if (this.listMenuMobile!.nativeElement.classList.contains('open-menu')) {
+      this.listMenuMobile!.nativeElement.classList.replace('open-menu', 'close-menu');
+      this.containerLabel!.nativeElement.style.background = '#FFFFFF';
+    } else {
+      this.containerLabel!.nativeElement.style.background = '#EB9800';
+      this.listMenuMobile!.nativeElement.classList.replace('close-menu', 'open-menu');
+    }
   }
 
 }
